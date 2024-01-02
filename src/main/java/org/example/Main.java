f0fdac99e94aab3d4b25df4a9039fba3460eccd3
a3rzi <amirhosseinqatar@gmail.com>
2024-01-02 22:04:23 +0330

    step1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("inter a mounth(1 to 12): ");
+        int month = input.nextInt();
+
     }
 }
